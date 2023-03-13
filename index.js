3ffabc28916d8aea688145352924e6e7dc6a04b1
Richard Sobreiro <richardsobreiro@gmail.com>
2023-03-13 12:07:41 -0300

    FIT: react native app

--- index.js
+++ index.js
@@ -46,3 +46,36 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2((slideIndex2 += n));
+}
+
+function currentSlide2(n) {
+  showSlides2((slideIndex2 = n));
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide2");
+  let dots = document.getElementsByClassName("demo2");
+  let captionText = document.getElementById("caption2");
+  if (n > slides.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2 - 1].style.display = "block";
+  dots[slideIndex2 - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex2 - 1].alt;
+}
